d6eaf5dda1621f08caf7f0b06e28905225227c0c
Erika Pauwels <erika.pauwels@tenforce.com>
2015-07-03 14:36:46 +0200

    Add live reload in development environment

--- startup.sh
+++ startup.sh
@@ -2,8 +2,11 @@
 if [ "$RACK_ENV" == "production" ]; 
 then 
   bundle install --without development test
+  ruby $MAIN_APP_FILE -p 80
 else
   bundle install
+  gem install shotgun
+  shotgun $MAIN_APP_FILE -p 80 -o '0.0.0.0'
 fi
 
-ruby $MAIN_APP_FILE -p 80
+
